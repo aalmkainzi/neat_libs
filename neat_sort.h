118dc90ae7d58e7e1f4c940c23fda1bade6f2f00
aalmkainzi <aalmkainzi@gmail.com>
2023-09-03 23:44:07 +0300

    using stdint.h types now. Improved some of the macro names

--- neat_sort.h
+++ neat_sort.h
@@ -3,39 +3,36 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
-#define ADD_SORT_TYPE(type, cmp_function) type*: cmp = (cmp_func) cmp_function
+#define ADD_SORTABLE(type, cmp_function) type*: cmp = (cmp_func) cmp_function
 
-#ifdef ADDITIONAL_SORT_TYPES
+#ifdef SORTABLE_TYPES
 
 #define SORT_TYPES \
-ADD_SORT_TYPE(unsigned char,      uchar_cmp), \
-ADD_SORT_TYPE(unsigned short,     ushort_cmp), \
-ADD_SORT_TYPE(unsigned int,       uint_cmp), \
-ADD_SORT_TYPE(unsigned long,      ulong_cmp), \
-ADD_SORT_TYPE(unsigned long long, ullong_cmp), \
-ADD_SORT_TYPE(char,               char_cmp), \
-ADD_SORT_TYPE(short,              short_cmp), \
-ADD_SORT_TYPE(int,                int_cmp), \
-ADD_SORT_TYPE(long,               long_cmp), \
-ADD_SORT_TYPE(long long,          llong_cmp), \
-ADD_SORT_TYPE(char*,              str_cmp), \
-ADDITIONAL_SORT_TYPES
+ADD_SORTABLE(uint8_t,  neat_uint8_t_cmp), \
+ADD_SORTABLE(uint16_t, neat_uint16_t_cmp), \
+ADD_SORTABLE(uint32_t, neat_uint32_t_cmp), \
+ADD_SORTABLE(uint64_t, neat_uint64_t_cmp), \
+ADD_SORTABLE(int8_t,   neat_int8_t_cmp), \
+ADD_SORTABLE(int16_t,  neat_int16_t_cmp), \
+ADD_SORTABLE(int32_t,  neat_int32_t_cmp), \
+ADD_SORTABLE(int64_t,  neat_int64_t_cmp), \
+ADD_SORTABLE(char*,    str_cmp), \
+SORTABLE_TYPES
 
 #else
 
 #define SORT_TYPES \
-ADD_SORT_TYPE(unsigned char,      uchar_cmp), \
-ADD_SORT_TYPE(unsigned short,     ushort_cmp), \
-ADD_SORT_TYPE(unsigned int,       uint_cmp), \
-ADD_SORT_TYPE(unsigned long,      ulong_cmp), \
-ADD_SORT_TYPE(unsigned long long, ullong_cmp), \
-ADD_SORT_TYPE(char,               char_cmp), \
-ADD_SORT_TYPE(short,              short_cmp), \
-ADD_SORT_TYPE(int,                int_cmp), \
-ADD_SORT_TYPE(long,               long_cmp), \
-ADD_SORT_TYPE(long long,          llong_cmp), \
-ADD_SORT_TYPE(char*,              str_cmp) \
+ADD_SORTABLE(uint8_t,  neat_uint8_t_cmp), \
+ADD_SORTABLE(uint16_t, neat_uint16_t_cmp), \
+ADD_SORTABLE(uint32_t, neat_uint32_t_cmp), \
+ADD_SORTABLE(uint64_t, neat_uint64_t_cmp), \
+ADD_SORTABLE(int8_t,   neat_int8_t_cmp), \
+ADD_SORTABLE(int16_t,  neat_int16_t_cmp), \
+ADD_SORTABLE(int32_t,  neat_int32_t_cmp), \
+ADD_SORTABLE(int64_t,  neat_int64_t_cmp), \
+ADD_SORTABLE(char*,    str_cmp) \
 
 #endif
 
@@ -58,30 +55,19 @@ _Generic(arr, \
 qsort(arr, n, sizeof(*arr), cmp); \
 } while(0)
 
-
-#define declare_number_cmp_func(type) int type##_cmp(const type *a, const type *b)
+#define declare_number_cmp_func(type) int neat_##type##_cmp (const type *a, const type *b)
 #define define_number_cmp_func(type) declare_number_cmp_func(type) { return (*a > *b) - (*b > *a); }
-
-// these typedefs are for multi word types, for easy cmp function creation
-typedef unsigned char      uchar;
-typedef unsigned short     ushort;
-typedef unsigned int       uint;
-typedef unsigned long      ulong;
-typedef long long          llong;
-typedef unsigned long long ullong;
 
 // compare functions to pass to qsort
 #ifdef NEAT_SORT_IMPLEMENTATION
-define_number_cmp_func(char);
-define_number_cmp_func(uchar);
-define_number_cmp_func(short);
-define_number_cmp_func(ushort);
-define_number_cmp_func(int);
-define_number_cmp_func(uint);
-define_number_cmp_func(long);
-define_number_cmp_func(ulong);
-define_number_cmp_func(llong);
-define_number_cmp_func(ullong);
+define_number_cmp_func(int8_t);
+define_number_cmp_func(uint8_t);
+define_number_cmp_func(int16_t);
+define_number_cmp_func(uint16_t);
+define_number_cmp_func(int32_t);
+define_number_cmp_func(uint32_t);
+define_number_cmp_func(int64_t);
+define_number_cmp_func(uint64_t);
 define_number_cmp_func(float);
 define_number_cmp_func(double);
 
@@ -91,16 +77,14 @@ int str_cmp(const char **s1, const char **s2)
 }
 
 #else
-declare_number_cmp_func(char);
-declare_number_cmp_func(uchar);
-declare_number_cmp_func(short);
-declare_number_cmp_func(ushort);
-declare_number_cmp_func(int);
-declare_number_cmp_func(uint);
-declare_number_cmp_func(long);
-declare_number_cmp_func(ulong);
-declare_number_cmp_func(llong);
-declare_number_cmp_func(ullong);
+declare_number_cmp_func(int8_t);
+declare_number_cmp_func(uint8_t);
+declare_number_cmp_func(int16_t);
+declare_number_cmp_func(uint16_t);
+declare_number_cmp_func(int32_t);
+declare_number_cmp_func(uint32_t);
+declare_number_cmp_func(int64_t);
+declare_number_cmp_func(uint64_t);
 declare_number_cmp_func(float);
 declare_number_cmp_func(double);
 int str_cmp(const char **s1, const char **s2);
