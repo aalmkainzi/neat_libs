aa1fc7e8fd38c1c133bb8db829cf27150fd5e06e
aalmkainzi <aalmkainzi@gmail.com>
2023-09-07 12:00:58 +0300

    Made BSEARCH able to take literals. refactored some stuff

--- neat_sort.h
+++ neat_sort.h
@@ -21,16 +21,16 @@ ADD_SORTABLE(double,   neat_double_cmp), \
 ADD_SORTABLE(char*,    neat_str_cmp)
 
 #ifdef SORTABLE_TYPES
-
-#define ALL_SORTABLE_TYPES \
-DEFAULT_SORTABLE_TYPES, \
-SORTABLE_TYPES
-
-#else
-
-#define ALL_SORTABLE_TYPES \
-DEFAULT_SORTABLE_TYPES
-
+    
+    #define ALL_SORTABLE_TYPES \
+    DEFAULT_SORTABLE_TYPES, \
+    SORTABLE_TYPES
+    
+#else // SORTABLE_TYPES
+    
+    #define ALL_SORTABLE_TYPES \
+    DEFAULT_SORTABLE_TYPES
+    
 #endif // SORTABLE_TYPES
 
 typedef int (*cmp_func)(const void*, const void*);
@@ -39,10 +39,7 @@ typedef int (*cmp_func)(const void*, const void*);
 
 #define GET_CMP_FOR(type) ((cmp_func) _Generic(type, ALL_SORTABLE_TYPES))
 
-#define SORT_PTR(arr, n) do { \
-cmp_func cmp = GET_CMP_FOR(*arr); \
-qsort(arr, n, sizeof(*arr), cmp); \
-} while(0)
+#define SORT_PTR(arr, n) qsort(arr, n, sizeof(*arr), GET_CMP_FOR(*arr))
 
 #define SORT(arr) SORT_PTR(arr, ARR_LEN(arr))
 
@@ -63,11 +60,11 @@ REVERSE_ARRAY_PTR(arr, n); \
 
 #define SORT_DESC(arr) SORT_DESC_PTR(arr, ARR_LEN(arr))
 
-#define BSEARCH_PTR(arr, n, key) bsearch (&key, arr, n, sizeof(*arr), GET_CMP_FOR(*arr)) \
+#define BSEARCH_PTR(arr, n, key) bsearch (&(typeof(*arr)[]){key}[0], arr, n, sizeof(*arr), GET_CMP_FOR(*arr)) \
 
 #define BSEARCH(arr, key) BSEARCH_PTR(arr, ARR_LEN(arr), key)
 
-#define SEARCH_PTR(arr, n, key) neat_search(&key, arr, n, sizeof(*arr), GET_CMP_FOR(*arr))
+#define SEARCH_PTR(arr, n, key) neat_search(&(typeof(*arr)[]){key}[0], arr, n, sizeof(*arr), GET_CMP_FOR(*arr))
 
 #define SEARCH(arr, key) SEARCH_PTR(arr, ARR_LEN(arr), key)
 
@@ -76,48 +73,50 @@ REVERSE_ARRAY_PTR(arr, n); \
 
 // default compare functions to pass to qsort/bsearch
 #ifdef NEAT_SORT_IMPLEMENTATION
-define_number_cmp_func(int8_t);
-define_number_cmp_func(uint8_t);
-define_number_cmp_func(int16_t);
-define_number_cmp_func(uint16_t);
-define_number_cmp_func(int32_t);
-define_number_cmp_func(uint32_t);
-define_number_cmp_func(int64_t);
-define_number_cmp_func(uint64_t);
-define_number_cmp_func(float);
-define_number_cmp_func(double);
-
-int neat_str_cmp(const char **s1, const char **s2)
-{
-    return strcmp(*s1, *s2);
-}
-
-void *neat_search(const void *key, const void *base, size_t nmemb, size_t elm_size, cmp_func cmp)
-{
-    const uint8_t *arr = (const uint8_t *) base;
-    for(size_t i = 0 ; i < nmemb ; i++)
+    
+    define_number_cmp_func(int8_t);
+    define_number_cmp_func(uint8_t);
+    define_number_cmp_func(int16_t);
+    define_number_cmp_func(uint16_t);
+    define_number_cmp_func(int32_t);
+    define_number_cmp_func(uint32_t);
+    define_number_cmp_func(int64_t);
+    define_number_cmp_func(uint64_t);
+    define_number_cmp_func(float);
+    define_number_cmp_func(double);
+    
+    int neat_str_cmp(const char **s1, const char **s2)
     {
-        const uint8_t *elm = arr + i * elm_size;
-        if(cmp(key, elm) == 0) return elm;
+        return strcmp(*s1, *s2);
     }
-    return NULL;
-}
-
-#else
-declare_number_cmp_func(int8_t);
-declare_number_cmp_func(uint8_t);
-declare_number_cmp_func(int16_t);
-declare_number_cmp_func(uint16_t);
-declare_number_cmp_func(int32_t);
-declare_number_cmp_func(uint32_t);
-declare_number_cmp_func(int64_t);
-declare_number_cmp_func(uint64_t);
-declare_number_cmp_func(float);
-declare_number_cmp_func(double);
-int neat_str_cmp(const char **s1, const char **s2);
-
-void *neat_search(const void *key, const void *base, size_t nmemb, size_t elm_size, cmp_func cmp);
-
+    
+    void *neat_search(const void *key, const void *base, size_t nmemb, size_t elm_size, cmp_func cmp)
+    {
+        const uint8_t *arr = (const uint8_t *) base;
+        for(size_t i = 0 ; i < nmemb ; i++)
+        {
+            const uint8_t *elm = arr + i * elm_size;
+            if(cmp(key, elm) == 0) return elm;
+        }
+        return NULL;
+    }
+    
+#else // NEAT_SORT_IMPLEMENTATION
+    
+    declare_number_cmp_func(int8_t);
+    declare_number_cmp_func(uint8_t);
+    declare_number_cmp_func(int16_t);
+    declare_number_cmp_func(uint16_t);
+    declare_number_cmp_func(int32_t);
+    declare_number_cmp_func(uint32_t);
+    declare_number_cmp_func(int64_t);
+    declare_number_cmp_func(uint64_t);
+    declare_number_cmp_func(float);
+    declare_number_cmp_func(double);
+    int neat_str_cmp(const char **s1, const char **s2);
+    
+    void *neat_search(const void *key, const void *base, size_t nmemb, size_t elm_size, cmp_func cmp);
+    
 #endif // NEAT_SORT_IMPLEMENTATION
 
 #endif // NEAT_SORT_H
